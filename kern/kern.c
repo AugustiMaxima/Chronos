03348c489b668b1c1c3ddd17e5ce911f21c813f7
Lennox Fei <f5fei@uwaterloo.ca>
2019-09-25 00:18:23 -0400

    Revert "Prologue removal -- attempt 2"
    This reverts commit 56db076f2bfce2a4e42489d16dfdf1cc84ccbfbb.

--- kern/kern.c
+++ kern/kern.c
@@ -2,93 +2,42 @@
 #include <bwio.h>
 #include <dump.h>
 
-
-__attribute__((naked)) void exitKernel(void* processStackPtr){
+void exitKernel(void* processStackPtr){
 
     // save kernel registers on kernel stack
-    asm("SUB SP, SP, #64");
-    asm("STR R0, [SP]");
-    asm("STR R1, [SP, #4]");
-    asm("STR R2, [SP, #8]");
-    asm("STR R3, [SP, #12]");
-    asm("STR R4, [SP, #16]");
-    asm("STR R5, [SP, #20]");
-    asm("STR R6, [SP, #24]");
-    asm("STR R7, [SP, #28]");
-    asm("STR R8, [SP, #32]");
-    asm("STR R9, [SP, #36]");
-    asm("STR R10, [SP, #40]");
-    asm("STR R11, [SP, #44]");
-    asm("STR R12, [SP, #48]");
-    asm("STR R13, [SP, #52]");
-    asm("STR R14, [SP, #56]");
-    asm("ADD R2, PC, #20");
-    //sets where the return address should be
-    asm("STR R2, [SP, #60]");
+    asm("stmfd sp!, {r0-r12, lr}");
+
 
     // //register level memory dump
-    void* psp = processStackPtr;
-    bwprintf(COM2, "Stackptr: \t %x\r\n", psp);
+    // void* psp = processStackPtr;
+    // bwprintf(COM2, "Stackptr: \t %x\r\n", psp);
 
 
-    int i;
-    int temp;
-    for(i=0;i<17;i++){
-        asm("ldr %0, [%1]" :"=r"(temp):"r"(psp));
-        bwprintf(COM2, "R%d \t %x at %d\r\n", 16-i, temp, psp);
-        psp += 4;
-    }
+    // int i;
+    // int temp;
+    // for(i=0;i<17;i++){
+    //     asm("ldr %0, [%1]" :"=r"(temp):"r"(psp));
+    //     bwprintf(COM2, "R%d \t %x at %d\r\n", 16-i, temp, psp);
+    //     psp += 4;
+    // }
 
 
     //change to user mode
     // cpsr <- *processStackPtr
 
-    asm("LDR R1, [R0]");
-    asm("MSR CPSR_c, R1");
+    asm("ldr r1, [r0]");
+    asm("msr cpsr_c, r1");
 
     //restores the stack pointer, minus the cpsr
     // sp <- r0 - 4
-    asm("ADD SP, R0, #4");
+    asm("add sp, r0, #4");
 
     //loads user mode registers
     //includes the PC register, and starts executing
-    asm("LDR R0, [SP]");
-    asm("LDR R1, [SP, #4]");
-    asm("LDR R2, [SP, #8]");
-    asm("LDR R3, [SP, #12]");
-    asm("LDR R4, [SP, #16]");
-    asm("LDR R5, [SP, #20]");
-    asm("LDR R6, [SP, #24]");
-    asm("LDR R7, [SP, #28]");
-    asm("LDR R8, [SP, #32]");
-    asm("LDR R9, [SP, #36]");
-    asm("LDR R10, [SP, #40]");
-    asm("LDR R11, [SP, #44]");
-    asm("LDR R12, [SP, #48]");
-    asm("LDR R13, [SP, #52]");
-    asm("LDR R14, [SP, #56]");
-    asm("SUB SP, SP, #64");
-    asm("LDR R15, [SP, #-4]");
-
-    asm("MOV PC, LR");
+    asm("ldmfd SP!, {r0-pc}");
 }
 
-__attribute__((naked)) void enterKernel(){
-    asm("LDR R0, [SP]");
-    asm("LDR R1, [SP, #4]");
-    asm("LDR R2, [SP, #8]");
-    asm("LDR R3, [SP, #12]");
-    asm("LDR R4, [SP, #16]");
-    asm("LDR R5, [SP, #20]");
-    asm("LDR R6, [SP, #24]");
-    asm("LDR R7, [SP, #28]");
-    asm("LDR R8, [SP, #32]");
-    asm("LDR R9, [SP, #36]");
-    asm("LDR R10, [SP, #40]");
-    asm("LDR R11, [SP, #44]");
-    asm("LDR R12, [SP, #48]");
-    asm("LDR R13, [SP, #52]");
-    asm("LDR R14, [SP, #56]");
-    asm("SUB SP, SP, #64");
-    asm("LDR R15, [SP, #-4]");
+void enterKernel(){
+    asm("LDMFD SP!, {R0-R12, LR}");
+    asm("MOV PC, LR");
 }
