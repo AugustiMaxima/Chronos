1a8784c0818ea0d4a0cf07f8fe4d0e4ad6e79938
Lennox Fei <ffei1314@gmail.com>
2019-09-24 23:23:01 -0400

    sys

--- kern/syscall.c
+++ kern/syscall.c
@@ -16,10 +16,11 @@ __attribute__((naked)) void sys_handler(){
 
     switch (code){
         case 0:
-            getTid();
+            sysGetTId();
         case 1:
             sysCreateTask();
         default:
+            ;
     }
     
     void* jump = handleSuspendedTasks;
@@ -48,8 +49,8 @@ __attribute__((naked)) void sysCreateTask(){
     asm("MSR CPSR, R0");
 
     //arguments are stored and retrieved in ascending order
-    asm("LDR %1, [SP]", :: "r" (funcPtr));
-    asm("LDR %1, [SP, 4]", :: "r" (priority));
+    asm("LDR %1, [SP]" :: "r" (funcPtr));
+    asm("LDR %1, [SP, 4]" :: "r" (priority));
     asm("ADD SP, SP, #8");
     
     asm("MRS R0, CPSR");
