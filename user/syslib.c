31c218cff23f9cd4d4ab387a61ce036ca084134e
Xuanji Li <x538li@uwaterloo.ca>
2019-09-28 17:30:21 -0400

    create save_user_context

--- user/syslib.c
+++ user/syslib.c
@@ -3,7 +3,7 @@
 #include <bwio.h>
 #include <scheduler.h>
 
-int Create(int priority, void (*function)()){
+static inline __attribute__((always_inline)) int save_user_context() {
     asm("SUB SP, SP, #64");
     asm("STR R0, [SP]");
     asm("STR R1, [SP, #4]");
@@ -23,106 +23,36 @@ int Create(int priority, void (*function)()){
     asm("SUB SP, SP, #4");
     asm("MRS R2, CPSR");
     asm("STR R2, [SP]");
+}
+
+int Create(int priority, void (*function)()){
+    save_user_context();
     //argument stuffing now
     asm("SUB SP, SP, #8");
     asm("STR R0, [SP]");
     asm("STR R1, [SP, #4]");
-    // int* stack;
-    // asm("MOV %0, SP":"=r"(stack));
-    // printRegisters(stack+2);
+
     asm("SWI " TOSTRING(CREATE_CODE));
     //<---- PC return points here
 }
 
 int MyTid(){
-    asm("SUB SP, SP, #64");
-    asm("STR R0, [SP]");
-    asm("STR R1, [SP, #4]");
-    asm("STR R2, [SP, #8]");
-    asm("STR R3, [SP, #12]");
-    asm("STR R4, [SP, #16]");
-    asm("STR R5, [SP, #20]");
-    asm("STR R6, [SP, #24]");
-    asm("STR R7, [SP, #28]");
-    asm("STR R8, [SP, #32]");
-    asm("STR R9, [SP, #36]");
-    asm("STR R10, [SP, #40]");
-    asm("STR R11, [SP, #44]");
-    asm("STR R12, [SP, #48]");
-    asm("STR R13, [SP, #52]");
-    asm("STR R14, [SP, #56]");
-    asm("SUB SP, SP, #4");
-    asm("MRS R2, CPSR");
-    asm("STR R2, [SP]");
+    save_user_context();
     asm("SWI " TOSTRING(MYTID_CODE));
 }
 
 int MyParentTid(){
-    asm("SUB SP, SP, #64");
-    asm("STR R0, [SP]");
-    asm("STR R1, [SP, #4]");
-    asm("STR R2, [SP, #8]");
-    asm("STR R3, [SP, #12]");
-    asm("STR R4, [SP, #16]");
-    asm("STR R5, [SP, #20]");
-    asm("STR R6, [SP, #24]");
-    asm("STR R7, [SP, #28]");
-    asm("STR R8, [SP, #32]");
-    asm("STR R9, [SP, #36]");
-    asm("STR R10, [SP, #40]");
-    asm("STR R11, [SP, #44]");
-    asm("STR R12, [SP, #48]");
-    asm("STR R13, [SP, #52]");
-    asm("STR R14, [SP, #56]");
-    asm("SUB SP, SP, #4");
-    asm("MRS R2, CPSR");
-    asm("STR R2, [SP]");
+    save_user_context();
     asm("SWI " TOSTRING(MYPARENTTID_CODE));
 }
 
 void Yield(){
-    asm("SUB SP, SP, #64");
-    asm("STR R0, [SP]");
-    asm("STR R1, [SP, #4]");
-    asm("STR R2, [SP, #8]");
-    asm("STR R3, [SP, #12]");
-    asm("STR R4, [SP, #16]");
-    asm("STR R5, [SP, #20]");
-    asm("STR R6, [SP, #24]");
-    asm("STR R7, [SP, #28]");
-    asm("STR R8, [SP, #32]");
-    asm("STR R9, [SP, #36]");
-    asm("STR R10, [SP, #40]");
-    asm("STR R11, [SP, #44]");
-    asm("STR R12, [SP, #48]");
-    asm("STR R13, [SP, #52]");
-    asm("STR R14, [SP, #56]");
-    asm("SUB SP, SP, #4");
-    asm("MRS R2, CPSR");
-    asm("STR R2, [SP]");
+    save_user_context();
     asm("SWI " TOSTRING(YIELD_CODE));
 }
 
 void Exit(){
-    asm("SUB SP, SP, #64");
-    asm("STR R0, [SP]");
-    asm("STR R1, [SP, #4]");
-    asm("STR R2, [SP, #8]");
-    asm("STR R3, [SP, #12]");
-    asm("STR R4, [SP, #16]");
-    asm("STR R5, [SP, #20]");
-    asm("STR R6, [SP, #24]");
-    asm("STR R7, [SP, #28]");
-    asm("STR R8, [SP, #32]");
-    asm("STR R9, [SP, #36]");
-    asm("STR R10, [SP, #40]");
-    asm("STR R11, [SP, #44]");
-    asm("STR R12, [SP, #48]");
-    asm("STR R13, [SP, #52]");
-    asm("STR R14, [SP, #56]");
-    asm("SUB SP, SP, #4");
-    asm("MRS R2, CPSR");
-    asm("STR R2, [SP]");
+    save_user_context();
     asm("SWI " TOSTRING(EXIT_CODE));
 }
 
