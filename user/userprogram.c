2df17dad053afb9c321de19d0d1f98e4a90de645
Xuanji Li <x538li@uwaterloo.ca>
2019-09-29 20:07:34 -0400

    remove duplicate

--- user/userprogram.c
+++ user/userprogram.c
@@ -4,25 +4,6 @@
 #include <stdlib.h>
 #include <map.h>
 
-void k1_child() {
-    bwprintf(COM2, "%d %d\r\n", MyTid(), MyParentTid());
-    Yield();
-    bwprintf(COM2, "%d %d\r\n", MyTid(), MyParentTid());
-    Exit();
-}
-
-void k1_main(){
-    int ret = Create(-1, k1_child);
-    bwprintf(COM2, "Created: %d\r\n", ret);
-    ret = Create(-1, k1_child);
-    bwprintf(COM2, "Created: %d\r\n", ret);
-    ret = Create(1, k1_child);
-    bwprintf(COM2, "Created: %d\r\n", ret);
-    ret = Create(1, k1_child);
-    bwprintf(COM2, "Created: %d\r\n", ret);
-    bwprintf(COM2, "FirstUserTask: Exit\r\n");
-}
-
 void receiver() {
     int who;
     char buf[100];
