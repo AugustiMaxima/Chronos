b057225b9d5a78bdd49c2486e4f10409aafcc72a
Xuanji Li <x538li@uwaterloo.ca>
2019-10-02 12:21:36 -0400

    indentation

--- kernel.c
+++ kernel.c
@@ -25,43 +25,40 @@
 #define LOWER_MASK 0x0000ffff;
 
 int getTimerBase(int timer){
-	switch (timer){
-	    case 1:
-		return TIMER1_BASE;
-	    case 2:
-		return TIMER2_BASE;
-	    case 3:
-		return TIMER3_BASE;
-	}
+    switch (timer){
+        case 1: return TIMER1_BASE;
+        case 2: return TIMER2_BASE;
+        case 3: return TIMER3_BASE;
+    }
     return TIMER3_BASE;
 }
 
 unsigned int sanitizeLength(int timer, unsigned int length){
-	if (timer!=3){
-	    length &= LOWER_MASK;
-	}
-	return length;
+    if (timer!=3){
+        length &= LOWER_MASK;
+    }
+    return length;
 }
 
 void setFrequency(int timer, int frequency) {
     int* CRTL = getTimerBase(timer) + CRTL_OFFSET;
     switch (frequency){
     case 508000:
-	    *CRTL |= CLKSEL_MASK;
-	    break;
+        *CRTL |= CLKSEL_MASK;
+        break;
     default:
-	    *CRTL &= ~CLKSEL_MASK;
+        *CRTL &= ~CLKSEL_MASK;
     }
 }
 
 void setMode(int timer, int mode) {
     int* CRTL = getTimerBase(timer) + CRTL_OFFSET;
     switch (mode){
-    case 1: 
-	    *CRTL |= MODE_MASK;
-	break;
+    case 1:
+        *CRTL |= MODE_MASK;
+    break;
     default:
-	    *CRTL &= ~MODE_MASK;
+        *CRTL &= ~MODE_MASK;
     }
 }
 
@@ -80,16 +77,16 @@ unsigned int getValue(int timer) {
     int BASE = getTimerBase(timer);
     unsigned int value = *(int*)(BASE + VAL_OFFSET);
     if(timer!=3)
-	    value &= LOWER_MASK;
+        value &= LOWER_MASK;
     return value;
 }
 
 unsigned int getWrap(int timer) {
     switch (timer){
     case 3:
-	    return WRAP_32;
+        return WRAP_32;
     default:
-	    return WRAP_16;
+        return WRAP_16;
     }
 }
 //
