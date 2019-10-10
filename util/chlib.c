8e08c279c9bf7774fab30605132694da15a05160
Lennox Fei <f5fei@uwaterloo.ca>
2019-10-09 21:11:27 -0400

    Fixes memset

--- util/chlib.c
+++ util/chlib.c
@@ -13,12 +13,12 @@ void * memset ( void * ptr, int value, size_t num ){
     unsigned char value_downcast = value;
     value = value_downcast * 0x01010101;
     char* start = ptr;
-    char* end = ptr + num;
+    char* end = start + num;
     for(;(int)start%4 && start<end;start++){
         *start = value_downcast;
     }
     int* block = start;
-    for(;block + 1<end;block++){
+    for(;block + 1<=end;block++){
         *block = value;
     }
     start = block;
